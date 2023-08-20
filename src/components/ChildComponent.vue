00b286bb6530c4ad26cf8a4442fd66bd10b8cbc1
mashuk06 <mushfiqur.rahman06@gmail.com>
2023-08-20 21:33:15 +0600

    Assignment Complete

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { defineEmits, ref } from 'vue'
+
+defineProps(['message'])
+defineEmits(['childMessage'])
+
+const customMessage = "This is a message from child"
+
+</script>
+
+<template>
+  <div class="bg-gray-200 p-4 rounded-lg">
+    <h2 class="text-xl">Child Component</h2>
+    <p class="text-gray-700 font-bold">Received message from parent: </p>
+    <p class="text-gray-600">{{ message }}</p>
+    <button class="mt-2 px-4 py-2 bg-blue-500 text-white rounded" @click="$emit('messageEmitted', customMessage)">Emit Message</button>
+  </div>
+</template>
+
+
---
+++ src/components/ParentComponent.vue
@@ -0,0 +1,21 @@
+<script setup>
+    import ChildComponent from './ChildComponent.vue';
+    import { ref } from 'vue'
+
+    const parentMessage = ref('Hello from Parent')
+    const emittedMessage = ref('')
+
+    const handleChildMessage = (message) => {
+        emittedMessage .value = message
+    }
+</script>
+
+<template>
+    <div class="min-h-screen flex items-center justify-center bg-gray-100">
+        <div class="bg-white p-6 rounded-lg shadow-md">
+            <h2 class="text-xl my-3">Parent Component</h2>
+            <ChildComponent :message="parentMessage" @message-emitted="handleChildMessage" />
+            <p class="mt-4">Received message from child: {{ emittedMessage }}</p>
+        </div>
+    </div>
+</template>
